16f8273b26bf2db8f76fb7d9a0ded77e4e6768c9
Linne <finnlovejakell@gmail.com>
2022-06-22 15:44:05 +0800

    add: ✨ add solution for #10353

--- script/10353_workers_with_the_highest_salaries.sql
+++ script/10353_workers_with_the_highest_salaries.sql
@@ -1,9 +1,9 @@
 /*
 
-Question    : Workers With The Highest Salaries
+Title       : Workers With The Highest Salaries
 Link        : https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?code_type=1
 Difficulty  : Medium
-Description : Find the titles of workers that earn the highest salary. Output the highest-paid title or multiple titles that share the highest salary.
+Question    : Find the titles of workers that earn the highest salary. Output the highest-paid title or multiple titles that share the highest salary.
 Tables      : worker, title
 
 <worker>
@@ -31,6 +31,7 @@ WHERE w.salary = (
     FROM worker
 )
 
+
 -- solution 2: use dense_rank()
 SELECT worker_title
 FROM (
@@ -42,4 +43,22 @@ FROM (
 ) AS title_rank
 WHERE rk = 1
 
+
 -- soultion 3: use case when
+SELECT *
+FROM (
+    SELECT CASE 
+                WHEN w.salary = (SELECT MAX(Salary) FROM worker) THEN t.worker_title 
+           END AS highest_paid_title
+    FROM worker AS w
+    INNER JOIN title AS t
+        ON w.worker_id = t.worker_ref_id
+) AS highest_paid_title
+WHERE highest_paid_title IS NOT NULL
+
+
+-- Output:
+|highest_paid_title|
+|------------------|
+|Manager           |
+|Asst. Manager     |
