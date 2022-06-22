c45d04b824b58d348bd0e2c8d15d96f23d2c6faf
Linne <finnlovejakell@gmail.com>
2022-06-22 15:23:49 +0800

    add: ✨ add solution for #10353

--- script/10353_workers_with_the_highest_salaries.sql
+++ script/10353_workers_with_the_highest_salaries.sql
@@ -22,20 +22,24 @@ affected_from           datetime
 */
 
 -- solution 1: use max()
-select t.worker_title
-from worker as w
-inner join title as t
-on w.worker_id = t.worker_ref_id
-where w.salary = (
-    select max(salary)
-    from worker
+SELECT t.worker_title
+FROM worker AS w
+INNER JOIN title AS t
+    ON w.worker_id = t.worker_ref_id
+WHERE w.salary = (
+    SELECT MAX(salary)
+    FROM worker
 )
 
--- solution 2: use rank()
-
-
-
-
-
+-- solution 2: use dense_rank()
+SELECT worker_title
+FROM (
+    SELECT t.worker_title,
+           dense_rank() over (ORDER BY w.salary DESC) AS rk
+    FROM worker AS w
+    INNER JOIN title AS t
+        ON w.worker_id = t.worker_ref_id
+) AS title_rank
+WHERE rk = 1
 
 -- soultion 3: use case when
