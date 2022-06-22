97d720bf908ced77f0202885f87e1a2dc4058e67
Linne <finnlovejakell@gmail.com>
2022-06-22 14:55:53 +0800

    Update 10353_workers_with_the_highest_salaries.sql

--- script/10353_workers_with_the_highest_salaries.sql
+++ script/10353_workers_with_the_highest_salaries.sql
@@ -1,9 +1,11 @@
 /*
-Workers With The Highest Salaries
-https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?python=
-Difficulty: Medium
-Find the titles of workers that earn the highest salary. Output the highest-paid title or multiple titles that share the highest salary.
-Tables:
+
+Question    : Workers With The Highest Salaries
+Link        : https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?code_type=1
+Difficulty  : Medium
+Description : Find the titles of workers that earn the highest salary. Output the highest-paid title or multiple titles that share the highest salary.
+Tables      : worker, title
+
 <worker>
 worker_id               int
 first_name              varchar
@@ -11,8 +13,10 @@ last_name               varchar
 salary                  int
 joining_date            datetime
 department              varchar
-title
+
+<title>
 worker_ref_id           int
 worker_title            varchar
 affected_from           datetime
 */
+
