e9dd9461d12b3051caaa67914023d6f9f5816a46
Linne <finnlovejakell@gmail.com>
2022-06-26 19:23:46 +0800

    add: ✨ solution for #10299

---
+++ script/10299_finding_updated_records.sql
@@ -0,0 +1,47 @@
+/*
+
+Title       : Finding Updated Records
+Link        : https://platform.stratascratch.com/coding/10299-finding-updated-records?code_type=1
+Difficulty  : Easy
+Question    : We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+              Find the current salary of each employee assuming that salaries increase each year. 
+              Output their id, first name, last name, department ID, and current salary. 
+              Order your list by employee ID in ascending order.
+Tables      : ms_employee_salary
+
+<ms_employee_salary>
+id                  int
+first_name          varchar
+last_name           varchar
+salary              int
+department_id       int
+
+*/
+
+
+-- solution 1: 
+WITH current_salary AS (
+    SELECT id, MAX(salary) AS current_salary
+    FROM ms_employee_salary
+    GROUP BY id
+) 
+SELECT DISTINCT cs.id, 
+       mes.first_name,
+       mes.last_name, 
+       mes.department_id, 
+       cs.current_salary
+FROM current_salary AS cs
+INNER JOIN ms_employee_salary AS mes
+    ON cs.id = mes.id
+    
+-- solution 2:
+SELECT id, 
+       first_name, 
+       last_name, 
+       department_id, 
+       MAX(salary) AS current_salary
+FROM ms_employee_salary
+GROUP BY id, first_name, last_name, department_id
+ORDER BY id
+
+ORDER BY cs.id
